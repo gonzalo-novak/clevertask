84347f80a90420c19e4a34f38675125ecad2cb8a
Roberto Piñón <51218118+gonzalo-novak@users.noreply.github.com>
2024-06-25 01:57:12 -0600

    Feat/login endpoint (#6)

---
+++ server/src/routes/login/index.ts
@@ -0,0 +1,45 @@
+import * as argon2 from "argon2";
+import { logger } from "../../app";
+import { User } from "../../models/User";
+import { UserLoginReqBody } from "./types";
+import { Express, Request, Response } from "express";
+import { createAuthToken } from "../../utils/createAuthToken";
+import { verifyUserExistence } from "./middleware/verifyUserExistence";
+import { verifyUserLoginRequestFields } from "./middleware/verifyUserLoginRequestFields";
+
+const login = (app: Express) => {
+	app.post(
+		"/user/login",
+		[verifyUserLoginRequestFields, verifyUserExistence],
+		async (req: Request, res: Response) => {
+			const { email, password } = req.body as UserLoginReqBody;
+			const user = await User.findOne({ email: { $eq: email } });
+
+			try {
+				const {
+					password: userPassword,
+					email,
+					name,
+					lastName,
+				} = user!.toObject();
+				const isPasswordValid = await argon2.verify(userPassword, password);
+
+				if (!isPasswordValid) {
+					return res.status(401).json({
+						message: "The credentials are not correct. Try it again.",
+					});
+				}
+
+				const token = createAuthToken({ user: user!._id });
+				return res.json({ data: { user: { email, name, lastName }, token } });
+			} catch (error) {
+				logger.error(error);
+				return res.status(500).json({
+					message: "There was an error while processing the request",
+				});
+			}
+		}
+	);
+};
+
+export { login };
---
+++ server/src/routes/login/types.ts
@@ -0,0 +1,4 @@
+export type UserLoginReqBody = {
+	email: string;
+	password: string;
+};
