e49e02ad966012e4f388fed43b09035e6b16d897
Roberto Piñon <rpinon@hearsaycorp.com>
2024-06-25 01:21:02 -0600

    fix: NoSQL vulnerability

--- server/src/routes/register/middleware/validateEmailDuplication.ts
+++ server/src/routes/register/middleware/validateEmailDuplication.ts
@@ -2,14 +2,14 @@ import { NextFunction, Request, Response } from "express";
 import { User } from "../../../models/User";
 import { RegisterReqBody } from "../types";
 
-export const isEmailDuplicated = async (
+export const validateEmailDuplication = async (
 	req: Request,
 	res: Response,
 	next: NextFunction
 ) => {
 	const body: RegisterReqBody = req.body;
 	const { email } = body;
-	const emailDuplicated = await User.findOne({ email });
+	const emailDuplicated = await User.findOne({ email: { $eq: email } });
 
 	if (emailDuplicated) {
 		return res.status(500).json({
