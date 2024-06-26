e1b698657f607ed164f4311d2760f36fb3891ce8
Roberto Piñon <rpinon@hearsaycorp.com>
2024-06-26 01:17:44 -0600

    chore: configure the local proxy server

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { config } from "dotenv";
+
+config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	server: {
+		proxy: {
+			"/api": {
+				target: process.env.API_BASE_URL!,
+				rewrite: (path) => path.replace(/^\/api/, ""),
+			},
+		},
+	},
+});
