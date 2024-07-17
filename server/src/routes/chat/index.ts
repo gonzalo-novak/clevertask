3c695b822736b6341e822ee6e4abc07fd4a6daa7
Roberto Piñon <rpinon@hearsaycorp.com>
2024-07-17 01:01:02 -0600

    chore: generate the json object based on the conversation

--- server/src/routes/chat/index.ts
+++ server/src/routes/chat/index.ts
@@ -9,8 +9,13 @@ import { RedisClientType } from "redis";
 import { getUserIdFromToken } from "../../utils/getUserIdFromToken";
 import { Chat } from "../../models/Chat";
 import { Message } from "../../models/Message";
-import { Content, GoogleGenerativeAI } from "@google/generative-ai";
+import {
+	Content,
+	FunctionDeclarationSchemaType,
+	GoogleGenerativeAI,
+} from "@google/generative-ai";
 import { MAIN_INSTRUCTION, SYSTEM_INSTRUCTIONS } from "./constants";
+import { model } from "mongoose";
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY!);
 
@@ -18,13 +23,94 @@ const generationConfig = {
 	temperature: 0.25,
 	topP: 0.9,
 	topK: 40,
-	maxOutputTokens: 25000,
+	maxOutputTokens: 45000,
 	responseMimeType: "application/json",
 };
 
 let currentAction: string = "INIT";
 
 const chat = (app: Express, redis: RedisClientType) => {
+	app.get("/chat/:chatId/create_resources", authVerify, async (req, res) => {
+		try {
+			const chatId = req.params.chatId;
+			const userId = getUserIdFromToken(req);
+
+			if (!chatId) return res.sendStatus(401);
+
+			const chat = await Chat.find({ createdBy: userId, _id: chatId });
+
+			if (!chat) return res.sendStatus(401);
+
+			const messages = await Message.find({
+				chatId: chatId,
+			}).sort({ createdAt: 1 });
+
+			enum extractableActions {
+				"PURPOSE_DEFINITION" = "PURPOSE_DEFINITION",
+				"GOAL_TASK_DEFINITION" = "GOAL_TASK_DEFINITION",
+				"PURPOSE_GOAL_TASK_DEFINITION" = "PURPOSE_GOAL_TASK_DEFINITION",
+				"PURPOSE_GOAL_LIST_DEFINITION" = "PURPOSE_GOAL_LIST_DEFINITION",
+			}
+
+			const modelMessages = messages.filter(({ role }) => role === "model");
+
+			const purposeDefinition = modelMessages.filter(({ message }) =>
+				message.includes(extractableActions.PURPOSE_DEFINITION)
+			);
+
+			const purposeGoalListDefinition = modelMessages.filter(({ message }) =>
+				message.includes(extractableActions.PURPOSE_GOAL_LIST_DEFINITION)
+			);
+
+			const goalTaskDefinition = modelMessages.filter(
+				({ message }) =>
+					message.includes(extractableActions.GOAL_TASK_DEFINITION) ||
+					message.includes(extractableActions.PURPOSE_GOAL_TASK_DEFINITION)
+			);
+
+			const model = genAI.getGenerativeModel({
+				model: "gemini-1.5-pro",
+				generationConfig,
+			});
+
+			const { response } = await model.generateContent(`
+				Please, help me create a JSON based on a conversation that aims to create a plan for a goal. You don't have to summarize anything, just copy and paste the tasks for each section that I'll define below this message, however, try not to duplicate content. The tasks are already confirmed by the user, it's a matter of moving these tasks to the JSON object so I can store it in the database. First, we defined the main purpose of the user's goal, this is the answers the AI outputted:
+
+				PURPOSE_DEFINITION: ${purposeDefinition}
+
+				Also, we defined the goal list for that purpose:
+
+				PURPOSE_GOAL_LIST_DEFINITION: ${purposeGoalListDefinition}
+
+				Last but not least, we defined the task list for these goal list:
+
+				GOAL_TASK_DEFINITION: ${goalTaskDefinition}
+
+				Your task is output the following JSON schema based on the info above:
+
+				{
+					title: the purpose's title (from the PURPOSE_DEFINITION conversation),
+					description: the purposes description (from the PURPOSE_DEFINITION conversation),
+					goals: {
+						title: the goal title (from the PURPOSE_GOAL_LIST_DEFINITION conversation),
+						description: the goal description (from the PURPOSE_GOAL_LIST_DEFINITION conversation),
+						tasks: {
+							title: the goal task title (from the GOAL_TASK_DEFINITION conversation),
+							description: the goal task description (from the GOAL_TASK_DEFINITION conversation)
+						}[]
+					}[]
+				}
+			`);
+
+			const text = response.text();
+			console.log(response.usageMetadata);
+
+			return res.json({
+				text,
+			});
+		} catch (error) {}
+	});
+
 	app.post(
 		"/chat/:chatId/send",
 		[authVerify],
