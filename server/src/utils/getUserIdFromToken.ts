1d05e869f0cc0c97876aba9b1b9f8903865019db
Roberto Piñón <51218118+gonzalo-novak@users.noreply.github.com>
2024-07-27 21:28:22 -0600

    fix: method since it still was using express (#16)

--- server/src/utils/getUserIdFromToken.ts
+++ server/src/utils/getUserIdFromToken.ts
@@ -1,10 +1,7 @@
-import { Request } from "express";
 import jwt, { JwtPayload } from "jsonwebtoken";
 
-export const getUserIdFromToken = (req: Request) => {
-	const authToken = req.get("Authorization");
-
-	const { user: userId } = jwt.decode(authToken!) as JwtPayload & {
+export const getUserIdFromToken = (authToken: string) => {
+	const { user: userId } = jwt.decode(authToken) as JwtPayload & {
 		user: string;
 	};
 
